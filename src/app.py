07635ed261cf60eced01c42fa233efe64e614042
Ivan Bugaenko <bugaenko1234567890@yandex.ru>
2024-01-09 22:35:21 +0600

    sdfhdsljfhdlkjhfldhjflhjd

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from src.api.base_router import router
+from fastapi.middleware.cors import CORSMiddleware
 
 
 tag_description = [
@@ -9,11 +10,21 @@ tag_description = [
     }
 ]
 
+origins = ["*"]
+
 app = FastAPI(
     title='Приложение для перевода текста на различные языки',
     description='РГР по дисциплине NLP',
     version='0.0.1',
     openapi_tags=tag_description
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(router)
